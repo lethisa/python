72647323bed6b7f214a467259a57d995920d79a8
lethisa <lethisaputri@gmail.com>
2018-04-27 09:19:50 +0700

    add

--- playground/basic/exercise_06.py
+++ playground/basic/exercise_06.py
@@ -14,7 +14,7 @@ joke_evaluation = "Isn't that joke so funny ? %r"
 
 print joke_evaluation % hilarious
 
-w = "This is the left side of..."
-e = "a string with a right side."
+W = "This is the left side of..."
+E = "a string with a right side."
 
-print w + e
+print W + E
